41c31f771726b69aedc12f2956d6d03c59fe1056
hillyhills <hil.emenike@gmail.com>
2024-03-03 14:12:49 +0200

    backend configurations

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bucket-erply"
-    key = "public/terraform.tfstate"
-    region = "eu-north-1"
-    dynamodb_table = "terraform_state_locks"
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-state-bucket-erply"
+#     key = "public/terraform.tfstate"
+#     region = "eu-north-1"
+#     dynamodb_table = "terraform_state_locks"
     
-  }
-}
+#   }
+# }
