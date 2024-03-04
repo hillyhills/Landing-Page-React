d71601ab5922c29644fb23e5f61d708eeb22a945
hillyhills <hil.emenike@gmail.com>
2024-03-04 16:05:30 +0200

    implemented ingress egresss and security policy

--- terraform/modules/vpc_subnet/main.tf
+++ terraform/modules/vpc_subnet/main.tf
@@ -1,3 +1,4 @@
+#create VPC
 resource "aws_vpc" "website_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -5,6 +6,8 @@ resource "aws_vpc" "website_vpc" {
   }
 }
 
+
+#create subnet
 resource "aws_subnet" "subnet-1" {
   vpc_id            = aws_vpc.website_vpc.id
   cidr_block        = var.subnet_prefix[0].cidr_block
